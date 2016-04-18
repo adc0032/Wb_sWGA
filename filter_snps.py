d5b0a2e128688516ebe168e08f2a82f0c9a6c185
scott <stsmall@gmail.com>
2016-04-18 14:31:12 -0400

    add fisher strand

--- filter_snps.py
+++ filter_snps.py
@@ -6,6 +6,9 @@ filters SNPs from a freebayes file that has been normalized with vcflib and vt
 @author: stsmall
 """
 import sys
+from scipy import stats
+from math import log
+
 f = open(str(sys.argv[2]),'w')
 DPQ = []
 QbD = []
@@ -16,13 +19,18 @@ with open(str(sys.argv[1]),'r') as vcf:
             f.write(line)
         else:
             x = line.split()
-            if x[9].split(":")[0] in "0|1" or "0/1":
+            if x[9].split(":")[0] in "0|1" or "0/1" or "./1" or ".|1":
                 AB = x[7].split(";")[0]
                 DP = x[7].split(";")[7]
                 SAP = x[7].split(";")[35]
                 MQM = x[7].split(";")[15]
                 QUAL = x[6]
-                if (AB > 0.30 and AB < 0.70) and (QUAL > 30) and (MQM > 30) and (SAP < 60) and (DP > 20):
+                pvalue, oddsration = stats.fisher_exact([[x[7].split(";")[37].split("=")[1], x[7].split(";")[34].split("=")[1]],[x[7].split(";")[39].split("=")[1], x[7].split(";")[36].split("=")[1]]])
+                try:
+                    phred_pvalue = -10*(log(pvalue,10))  
+                except TypeError:
+                    phred_pvalue = 0
+                if (AB > 0.30 and AB < 0.70) and (QUAL > 30) and (MQM > 30) and (SAP < 60) and (DP > 20) and (phred_pvalue < 60):
                     f.write(line)
                     DPQ.append(DP)
                     QbD.append(QUAL)
@@ -33,6 +41,8 @@ with open(str(sys.argv[1]),'r') as vcf:
                 if (MQM > 30) and (DP > 20):
                     f.write(line)
 
+#FisherStrand 'FS <0.01' 
+#this is a pvalue, only from GATK FS > 60 (snp); FS > 200 (indel). 1. SRF 2. SRR 3. SAF 4. SAR
 avg_depth = float(sum(DPQ)/het_count)
 depth_thresh = avg_depth + 3*((avg_depth)**0.5)
 qualbdepth = [i for i,y in enumerate(QbD) if y < depth_thresh * 2]
