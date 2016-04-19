e56e2c588eb29f656014da45510c691a760cb68c
scott <stsmall@gmail.com>
2016-04-19 13:16:25 -0400

    final

--- fix_mnps.py
+++ fix_mnps.py
@@ -7,10 +7,8 @@ This script prints 2 lines. These can then be properly filtered.
 @author: stsmall
 """
 import sys,copy
-#with open(sys.argv[2],'w') as f:
-with open("fix_mnps.py.out",'w') as f:
-    #with open(sys.argv[1],'r') as vcf:
-    with open("test.mnp.out",'r') as vcf:
+with open(sys.argv[2],'w') as f:
+    with open(sys.argv[1],'r') as vcf:
         for line in vcf:
             if line.startswith("#"):
                 f.write(line)
@@ -18,29 +16,37 @@ with open("fix_mnps.py.out",'w') as f:
                 x = line.split()
                 if len(x[4]) > 2:
                     y = copy.copy(x)
-                    for i in range(x[7].split(";")):
-                        if "," in x[7][i]:
-                            x[7][i] = x[7][i].split("=")[0] + "=" + x[7][i].split("=").split(",")[0]
-                            y[7][i] = y[7][i].split("=")[0] + "=" + y[7][i].split("=").split(",")[0]
                     x[4] = x[4].split(",")[0]
-                    y[4] = y[4].split(",")[1]
-                    if "|" in x[9].split(":")[0]:
-                       x[9].split(":")[0] = "0|1"
-                       y[9].split(":")[0] = "0|1"
+                    y[4] = y[4].split(",")[1]                    
+                    x7 = x[7].split(";")
+                    y7 = y[7].split(";") 
+                    x9 = x[9].split(":")
+                    y9 = y[9].split(":")
+                    for i in range(len(x7)):
+                        if "," in x7[i]:                           
+                            x7[i] = x7[i].split("=")[0] + "=" + x7[i].split("=")[1].split(",")[0]
+                            y7[i] = y7[i].split("=")[0] + "=" + y7[i].split("=")[1].split(",")[1] 
+                    if "|" in x9[0]:
+                       x9[0] = "0|1"
+                       y9[0] = "0|1"
                     else:
-                        x[9].split(":")[0] = "0/1"
-                        y[9].split(":")[0] = "0/1"    
-                    x[9].split(":")[2] = ",".join(x[9].split(":")[2].split(",")[0],x[9].split(":")[2].split(",")[1])
-                    y[9].split(":")[2] = ",".join(y[9].split(":")[2].split(",")[0],y[9].split(":")[2].split(",")[2])
+                        x9[0] = "0/1"
+                        y9[0] = "0/1"    
+                    x9[2] = x9[2].split(",")[0] + "," + x9[2].split(",")[1]
+                    y9[2] = y9[2].split(",")[0] + "," + y9[2].split(",")[2]
                     
-                    x[9].split(":")[5] = x[9].split(":")[5].split(",")[0]
-                    y[9].split(":")[5] = y[9].split(":")[5].split(",")[1]
+                    x9[5] = x9[5].split(",")[0]
+                    y9[5] = y9[5].split(",")[1]
                     
-                    x[9].split(":")[6] = x[9].split(":")[6].split(",")[0]
-                    y[9].split(":")[6] = y[9].split(":")[6].split(",")[1]
+                    x9[6] = x9[6].split(",")[0]
+                    y9[6] = y9[6].split(",")[1]
                     
-                    x[9].split(":")[7] = ",".join(x[9].split(":")[7].split(",")[0], x[9].split(":")[7].split(",")[4], x[9].split(":")[7].split(",")[3])
-                    y[9].split(":")[7] = ",".join(y[9].split(":")[7].split(",")[0], y[9].split(":")[7].split(",")[4], y[9].split(":")[7].split(",")[5])
+                    x9[7] = x9[7].split(",")[0] + "," + x9[7].split(",")[4] + "," + x9[7].split(",")[3]
+                    y9[7] = y9[7].split(",")[0] + "," + y9[7].split(",")[4] + "," + y9[7].split(",")[5]
+                x[7] = ";".join(x7)
+                x[9] = ":".join(x9)
+                y[7] = ";".join(y7)
+                y[9] = ":".join(y9)
                 if line.split()[7].split(";")[0].split(",")[1] > line.split()[7].split(";")[0].split(",")[0].split("=")[1]:
                     f.write("\t".join(y) + "\n")                
                     f.write("\t".join(x) + "\n")
--- vcf_flatten.py
+++ vcf_flatten.py
@@ -8,19 +8,23 @@ This assumes that fix_mnps has selected the highest allele freq as the first lin
 """
 import sys
 with open(sys.argv[2],'w') as f:
-    with open(sys.argv[1],'r') as vcf:      
+   with open(sys.argv[1],'r') as vcf:     
         for line in vcf:
             if line.startswith("##"):
                 f.write(line)
             elif line.startswith("#CHROM"):
+                f.write(line)                
                 line = vcf.next()
                 contig = line.split()[0]
                 position = line.split()[1]
                 f.write(line)
             else:
                 x = line.split()
-                if x[1] == position and contig == x[0]:
+                if x[1] in position and contig in x[0]:
                     position = x[1]
+                    contig = x[0]
+                    pass
                 else:
-                    position = line.split()[1]                    
-                    f.write(line)
+                    f.write(line)                    
+                    position = x[1]
+                    contig = x[0]                    
