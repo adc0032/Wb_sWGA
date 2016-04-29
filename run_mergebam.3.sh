2b6dca673c3d3b29eec29ec358ce37f5e2956670
scott <stsmall@gmail.com>
2016-04-29 11:44:15 -0400

    asdf

--- run_mergebam.3.sh
+++ run_mergebam.3.sh
@@ -1,10 +1,9 @@
 #!bin/bash
-#ls *.bam |parallel -P 6 ./run_mergebam.sh {} {}
+#ls *.bam |parallel -P 6 run_mergebam.sh {} {}
 #--dry-run
 
 #merge bams from runs 1 and 2
 nice sambamba merge -t 10 /data/smalls/wuchereria/Wb_MF_swga_analysis/merged_bams/$( echo $1 | cut -d "." -f 1 ).rg.merge.bam $1 $2
 
 #index
-#ls *.merge.bam | parallel -P 6 ./run_index.sh {}
-#nice sambamba index -t 10 $( echo $1 | cut -d "." -f 1 ).rg.merge.bam
+#ls *.rg.merge.bam | parallel -P 6 sambamba index -t10 {}
--- run_snpcalling.4.sh
+++ run_snpcalling.4.sh
@@ -1,17 +1,17 @@
 #!bin/bash
-#ls *.rg.merge.bam | xargs -n 1 -P 21 run_snpcalling-fb.sh
+#ls *.rg.merge.bam | parallel -P 21 run_snpcalling.4.sh {}
 
-rgsm = $(echo $1 | cut -d "." -f 1)
+#rgsm = $(echo $1 | cut -d "." -f 1)
 
 ##bcftools/samtools
-nice samtools mpileup -ugf /SerreDLab/smalls/bowtie2_index/Wb-PNG_Genome_assembly-pt22.spades.ragoutrep.gapfill.mt.fasta $1 | bcftools call -mv -Ov > ${rgsm}.smt-all.vcf &
+nice samtools mpileup -ugf /SerreDLab/smalls/bowtie2_index/Wb-PNG_Genome_assembly-pt22.spades.ragoutrep.gapfill.mt.fasta $1 | bcftools call -mv -Ov > $(echo $1 | cut -d "." -f 1).smt-all.vcf &
 
 ##freebayes (parallel??)
-nice freebayes -f /SerreDLab/smalls/bowtie2_index/Wb-PNG_Genome_assembly-pt22.spades.ragoutrep.gapfill.mt.fasta --min-repeat-entropy 1 --min-alternate-count 2 --no-partial-observations --strict-vcf --genotype-qualities $1 > ${rgsm}.fb-all.vcf && fg
+nice freebayes -f /SerreDLab/smalls/bowtie2_index/Wb-PNG_Genome_assembly-pt22.spades.ragoutrep.gapfill.mt.fasta --min-repeat-entropy 1 --min-alternate-count 2 --no-partial-observations --strict-vcf --genotype-qualities $1 > $(echo $1 | cut -d "." -f 1).fb-all.vcf && fg
 #if running populations then invoke --no-population-priors or --pooled-discrete  and --theta
-grep -Fwvf contig-remove10k.out ${rgsm}.fb-all.vcf > ${rgsm}.fb.vcf
-vcffilter -f 'QUAL > 30' -s ${rgsm}.fb.vcf | vcfallelicprimitives --keep-geno --keep-info | vcffixup - | vcfstreamsort | vt normalize -r /SerreDLab/smalls/bowtie2_index/Wb-PNG_Genome_assembly-pt22.spades.ragoutrep.gapfill.mt.fasta -q - 2> /dev/null | vcfuniqalleles > ${rgsm}.norm.fb.vcf
-grep -Fwvf contig-remove10k.out ${rgsm}.smt-all.vcf > ${rgsm}.smt.vcf
+grep -Fwvf contig-remove10k.out $(echo $1 | cut -d "." -f 1).fb-all.vcf > $(echo $1 | cut -d "." -f 1).fb.vcf
+vcffilter -f 'QUAL > 30' -s $(echo $1 | cut -d "." -f 1).fb.vcf | vcfallelicprimitives --keep-geno --keep-info | vcffixup - | vcfstreamsort | vt normalize -r /SerreDLab/smalls/bowtie2_index/Wb-PNG_Genome_assembly-pt22.spades.ragoutrep.gapfill.mt.fasta -q - 2> /dev/null | vcfuniqalleles > $(echo $1 | cut -d "." -f 1).norm.fb.vcf
+grep -Fwvf contig-remove10k.out $(echo $1 | cut -d "." -f 1).smt-all.vcf > $(echo $1 | cut -d "." -f 1).smt.vcf
 #remove multimapping
 #~/programs_that_work/samtools/samtools view -h $1.rmd.bam | grep -v "XS:i:0" > $1.nomult.rmd.sam
 #~/programs_that_work/samtools/samtools view -Shb $1.nomult.rmd.sam > $1.nomult.rmd.bam
