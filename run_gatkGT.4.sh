4f0f4212da826b27d5c755bb423cf4aef859f670
scott <stsmall@gmail.com>
2016-04-29 12:17:06 -0400

    again

--- run_gatkGT.4.sh
+++ run_gatkGT.4.sh
@@ -11,5 +11,5 @@ module load GenomeAnalysisToolKit/3.5
 #ref.fasta,ref.fai,ref.dict, .bai
 
 #rgsm = $(echo *.${SLURM_ARRAY_TASK_ID}.rg.merge.bam | cut -d "." -f 1)
-srun java -jar /cm/shared/apps/GenomeAnalysisTk/3.5/GenomeAnalysisTK.jar -nt 20 -T GenotypeGVCFs -R Wb-PNG_Genome_assembly-pt22.spades.ragoutrep.gapfill.mt.fasta --variant $( ls gatk_gvcf/*.${SLURM_ARRAY_TASK_ID}.HC-all.gvcf | cut -d "." -f1 | cut -d "/" -f2 ).${SLURM_ARRAY_TASK_ID}.HC-all.gvcf -o $( ls gatk_gvcf/*.${SLURM_ARRAY_TASK_ID}.HC-all.gvcf | cut -d "." -f1 | cut -d "/" -f2 ).HC-all.vcf
+srun java -jar /cm/shared/apps/GenomeAnalysisTk/3.5/GenomeAnalysisTK.jar -nt 20 -T GenotypeGVCFs -R gatkHC/Wb-PNG_Genome_assembly-pt22.spades.ragoutrep.gapfill.mt.fasta --variant gatkHC/$( ls gatkHC/*.${SLURM_ARRAY_TASK_ID}.HC-all.g.vcf | cut -d "." -f1 | cut -d "/" -f2 ).${SLURM_ARRAY_TASK_ID}.HC-all.g.vcf -o $( ls gatkHC/*.${SLURM_ARRAY_TASK_ID}.HC-all.g.vcf | cut -d "." -f1 | cut -d "/" -f2 ).HC-all.vcf
 
--- run_gatkHC.4.sh
+++ run_gatkHC.4.sh
@@ -11,7 +11,7 @@ module load GenomeAnalysisToolKit/3.5
 #ref.fasta,ref.fai,ref.dict, merge.bam, merge.bam.bai
 #XXXX.N.rg.merge.bam; need to add #N between name and rg
 
-srun java -jar /cm/shared/apps/GenomeAnalysisTk/3.5/GenomeAnalysisTK.jar -nct 20 -T HaplotypeCaller -R Wb-PNG_Genome_assembly-pt22.spades.ragoutrep.gapfill.mt.fasta -I $( ls merge_bam/*.${SLURM_ARRAY_TASK_ID}.rg.merge.bam | cut -d "." -f1 | cut -d "/" -f2 ).${SLURM_ARRAY_TASK_ID}.rg.merge.bam --genotyping_mode DISCOVERY --pcr_indel_model NONE -pairHMM VECTOR_LOGLESS_CACHING --emitRefConfidence GVCF -o $( ls merge_bam/*.${SLURM_ARRAY_TASK_ID}.rg.merge.bam | cut -d "." -f1 | cut -d "/" -f2 ).${SLURM_ARRAY_TASK_ID}.HC-all.gvcf
+srun java -jar /cm/shared/apps/GenomeAnalysisTk/3.5/GenomeAnalysisTK.jar -nct 20 -T HaplotypeCaller -R Wb-PNG_Genome_assembly-pt22.spades.ragoutrep.gapfill.mt.fasta -I $( ls merge_bam/*.${SLURM_ARRAY_TASK_ID}.rg.merge.bam | cut -d "." -f1 | cut -d "/" -f2 ).${SLURM_ARRAY_TASK_ID}.rg.merge.bam --genotyping_mode DISCOVERY --pcr_indel_model NONE -pairHMM VECTOR_LOGLESS_CACHING --emitRefConfidence GVCF -o $( ls merge_bam/*.${SLURM_ARRAY_TASK_ID}.rg.merge.bam | cut -d "." -f1 | cut -d "/" -f2 ).${SLURM_ARRAY_TASK_ID}.HC-all.g.vcf
 
 #--heterozygosity .001
 #-stand_call_conf 30 -stand_emit_conf 10
