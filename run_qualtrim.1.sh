ba7dbaf90c6097c8db12350d686c7665703975cd
stsmall <stsmall@gmail.com>
2016-09-20 18:15:22 -0400

    Delete run_qualtrim.1.sh

--- run_qualtrim.1.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-#usage bash run_qualtrim.sh 
-#ls *.fastq.gz | xargs -n 2 -P 42 run_qualtrim.sh $1 $2
-trim_galore -q 20 --Illumina --paired --retain_unpaired -o /SerreDLab/smalls/wuchereria_bancrofti/Wb_MF_swga_analysis/Wb_swga_HiSeqx10_150bp $1s
