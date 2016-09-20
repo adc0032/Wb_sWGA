df9d67a85afaf48c38d4bd143be06e48d6afa09e
stsmall <stsmall@gmail.com>
2016-09-20 18:15:03 -0400

    Delete run_mergebam.3.sh

--- run_mergebam.3.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-#ls *.bam |parallel -P 6 run_mergebam.sh {} {}
-#--dry-run
-
-#merge bams from runs 1 and 2
-nice sambamba merge -t 10 /data/smalls/wuchereria/Wb_MF_swga_analysis/merged_bams/$( echo $1 | cut -d "." -f 1 ).rg.merge.bam $1 $2
-
-#index
-#ls *.rg.merge.bam | parallel -P 6 sambamba index -t10 {}
