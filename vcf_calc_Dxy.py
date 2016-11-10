ab196c257dd16a7be9e6453ed900195877da0963
smalls <stsmall@gmail.com>
2016-11-10 12:35:39 -0500

    arg!

--- vcf_calc_Dxy.py
+++ vcf_calc_Dxy.py
@@ -212,8 +212,8 @@ def DxyFromVcf(args):
     info_outfp = sys.stderr # where informative and summary info goes (must be set)
 
     # create the vcf.Reader object; pyVCF is way cool: http://pyvcf.readthedocs.org/en/latest/
-    print in_vcf
-    vcf_reader = vcf.Reader(open(in_vcf))
+    
+    vcf_reader = vcf.Reader(open("Wb.10k.54.recode.vcf",'r'))
 
     # verify samples in x and y groups are actually in the vcf
     for samp in set(x_samples+y_samples):
