3834af0f77d9f201d3f457fe6b5cc13354c4c4a7
smalls <stsmall@gmail.com>
2017-04-03 14:14:20 -0400

    t

--- stitchfillvcf.py
+++ stitchfillvcf.py
@@ -24,41 +24,48 @@ def stitch2vcf(vcf, stitch):
     impute = defaultdict(list)
     with open(stitch, 'r') as stitch:
         for line in stitch:
-            x = line.strip().split()
-            # assume all the same chromosome
-            impute[x[1]].append(x.split())
+            if line.startswith("#"):
+                pass
+            else:
+                x = line.strip().split()
+                # assume all the same chromosome
+                impute[x[1]].append(x.split())
 
     f = open('vcf', 'w')
 
     with open(vcf, 'r') as vcf:
         for line in vcf:
-            x = line.split()
-            # fill missing
-            wheremiss = re.match()
-            for missgt in wheremiss:
-                if impute[x[1]][missgt] == "./.":
-                    pass
-                else:
-                    fixgt = impute[x[1]][missgt].split(":")
-                    newgt = fixgt[0]
-                    if newgt == '0/0':
-                        fixgt[1] = "20,0"    # AD
-                    elif newgt == "0/1":
-                        fixgt[1] = "10,10"
+            if line.startswith("#"):
+                f.write(line)
+            else:
+                x = line.split()
+                # fill missing
+                miss = [i for i, s in enumerate(x) if re.search(r'\./\.', s)]
+                for missgt in miss:
+                    if impute[x[1]][missgt] == "./.":
+                        pass
                     else:
-                        fixgt[1] = "0,20"
-                fixgt[2] = "20"  # DP
-                fixgt[3] = "99"  # GQ
-                gltemp = [(-10*log10(float(a))) for a in fixgt[6].split(",")]
-                fixgt[6] = ",".join(map(str, gltemp))  # PL
-            x[wheremiss] = ":".join(fixgt)
-            # rewrite PL as GL; GL = PL/-10.0
-            for sample in range(9, len(x)):
-                gl = x[sample].split(":")
-                glnew = [float(a)/-10.0 for a in gl.split(",")]
-                gl[-1] = ",".join(map(str, glnew))
-                x[sample] = ":".join(gl)
-        f.write("{}\n".format("\t".join(x)))
+                        fixgt = impute[x[1]][missgt].split(":")
+                        newgt = fixgt[0]
+                        if newgt == '0/0':
+                            fixgt[1] = "20,0"    # AD
+                        elif newgt == "0/1":
+                            fixgt[1] = "10,10"
+                        else:
+                            fixgt[1] = "0,20"
+                    fixgt[2] = "20"  # DP
+                    fixgt[3] = "99"  # GQ
+                    gltemp = [(-10*log10(float(a)))
+                              for a in fixgt[6].split(",")]
+                    fixgt[6] = ",".join(map(str, gltemp))  # PL
+                x[miss] = ":".join(fixgt)
+                # rewrite PL as GL; GL = PL/-10.0
+                for sample in range(9, len(x)):
+                    gl = x[sample].split(":")
+                    glnew = [float(a)/-10.0 for a in gl.split(",")]
+                    gl[-1] = ",".join(map(str, glnew))
+                    x[sample] = ":".join(gl)
+            f.write("{}\n".format("\t".join(x)))
 
     f.close()
 
