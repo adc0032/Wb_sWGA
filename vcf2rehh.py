2151987e460cabfe9498f84fbd111e63c31ba3b8
smalls <stsmall@gmail.com>
2017-12-20 16:42:18 -0500

    update

--- vcf2rehh.py
+++ vcf2rehh.py
@@ -2,26 +2,53 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov  2 17:28:28 2017
-vcf2rehh.py
-makes a map.inp file for rehh assumes that there is an 'AA' field denoting the
-ancestral allele state in the VCF
+vcf2rehh.py -v VCF -ped PED -pops PNG Haiti Kenya Mali -s 5
 
-1st run: vcf2rehh.py -i vcf.AA -n chr
+Script to make a map.inp file for rehh assumes that there is an 'AA' field
+denoting the ancestral allele state in the VCF
+
+1st run: vcf2rehh.py
+2nd run: bcftools convert
+
 2nd run: plink --vcf phased.vcf --recode fastphase --out FOO --allow-extra-chr
 --double-id
-2nd a: bcftools convert
 
 @author: stsmall
 """
 import argparse
-import numpy as np
-from collections import defaultdict
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', "--vcf", type=str,
+parser.add_argument('-v', "--vcf", type=str,
                     help='path to vcf file')
-parser.add_argument('-ped', "--pedfile", type=str, required=True,
-                    help="path to pedfile with pop and individuals")
-parser.add_argument('-pops', "--poplist", type=str, nargs='+', required=True,
-                    help="list of pops")
-parser.add_argument('-s', "--size", type=str, nargs='+', required=False,
-                    default='', help="how many from each pop, blank uses all")
+args = parser.parse_args()
+
+
+def vcf2inp(vcfFile):
+    """
+    """
+    f = open(vcfFile + "inp", 'w')
+    with open(vcfFile, 'r') as vcf:
+        for line in vcf:
+            if line.startswith("#"):
+                pass
+            else:
+                x = line.strip().split()
+                fields = x[7].split(";")
+                snpid = "{}_{}".format(x[0], x[1])
+                if len(fields) > 1:
+                    anc = x[-1][-1]
+                else:
+                    anc = x[7][-1]
+                if anc == x[4]:
+                    der = x[3]
+                elif anc == x[3]:
+                    der = x[4]
+                else:
+                    anc = x[3]
+                    der = x[4]
+                f.write("{}/t{}/t{}/t{}/t{}/n".format(snpid, x[0], x[1], anc, der))
+    f.close()
+    return(None)
+
+
+if __name__ == "__main__":
+    vcf2inp(args.vcfFile)
