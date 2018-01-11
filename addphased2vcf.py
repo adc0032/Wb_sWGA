8af4fee4c269326589f51014f4b2a0f0b63d02c2
smalls <stsmall@gmail.com>
2018-01-11 13:31:33 -0500

    d

--- addphased2vcf.py
+++ addphased2vcf.py
@@ -48,18 +48,16 @@ def phased2vcf(vcf, phase):
                             gt_old = x[sample].split(":")
                             if "." not in gt_old[0]:
                                 gt_new = y[sample]
+                                gt_old[0] = gt_new
+                                x[sample] = ":".join(gt_old)
                             else:
-                                gt_new = ".|.:.:.:.:."
-                            gt_old[0] = gt_new
-                            x[sample] = ":".join(gt_old)
+                                gt_old = ".|.:.:.:.:."
                         f.write("{}\n".format("\t".join(x)))
                     except KeyError:
                         for sample in range(9, len(x)):
                             gt_old = x[sample].split(":")
                             if "." in gt_old[0]:
-                                gt_new = "./.:.:.:.:."
-                            gt_old[0] = gt_new
-                            x[sample] = ":".join(gt_old)
+                                gt_old = "./.:.:.:.:."
                         f.write("{}\n".format("\t".join(x)))
     f.close()
     return(None)
