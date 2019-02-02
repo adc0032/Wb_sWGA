2dfd1d59a5c0d1ba768ad411ad2a08ab07f191cc
smalls <stsmall@gmail.com>
2019-02-02 17:24:35 -0500

    convert ms to ped file for ADMIXTURE

---
+++ ms2ped.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  2 16:53:12 2019
+ms2ped --msFile FOO --thin 1000
+@author: stsmall
+"""
+from __future__ import print_function
+from __future__ import division
+import numpy as np
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--msFile", type=str, required=True,
+                    help="msformatted file")
+parser.add_argument("--locusLen", type=int, help="locus len")
+parser.add_argument("--thin", type=int, help="thin snps")
+args = parser.parse_args()
+
+
+def read_msformat_file(msFile, loclen, thin):
+    """Read and parse simulations from ms formatted file from folder of files
+    with basename
+    """
+    pos_list = []
+    pos_count = 0
+    gt_list = []
+    with open(msFile, 'r') as ms:
+        header = next(ms)
+        x = header.split()
+        nind = int(x[1])
+        nloci = int(x[2])
+        seed = int(next(ms.split()[0]))
+        for line in ms:
+            if line.startswith("positions"):
+                # collisions can result here when theta is high
+                pos = np.round(np.array(line.strip().split()[1:], dtype=np.float64) * loclen)
+                prev = 0
+                  for idx, item in enumerate(pos, start=0):
+                    while prev >= item:
+                        item += 1
+                    pos[idx] = item
+                    prev = pos[idx]
+                pos_list.append(pos.astype(np.int64) + pos_count)  # append
+                pos_count += block + 10000  # the two loci are unlinked
+                line = next(ms)
+                gt = np.zeros((nind, pos.shape[0]), dtype=np.uint8)
+                cix = 0
+                try:
+                    while line:
+                        line = list(line.strip())
+                        try:
+                            gt[cix, :] = np.array(line, dtype=np.uint8)
+                        except IndexError:
+                            break
+                        cix += 1
+                        line = next(ms)
+                except StopIteration:
+                    gt_list.append(gt)
+                    break
+                gt_list.append(gt)
+    return(gt_list, np.concatenate(pos_list, axis=0))
+
+
+def ms2ped(gt_list, pos_list):
+    """
+    """
+    f = open("sim.ped", 'w')
+    samp = 0
+    import ipdb;ipdb.set_trace()
+    for i in range():
+        f.write("Ind-{} 0 0 0 -9 {}\n".format(samp, ))
+        samp += 1
+
+
+    return(None)
+
+
+if __name__ == "__main__":
+    gt, pos = read_msformat_file(args.msFile, args.loclen, args.thin)
+    ms2ped(gt, pos)
